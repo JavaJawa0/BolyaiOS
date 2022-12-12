65b1d4d75f14dc999a7f18f36edd05a4e91219bd
Tóth Bálint Miksa <balinttoth28@gmail.com>
2022-12-12 14:15:27 +0100

    v2

--- src/main/main.java
+++ src/main/main.java
@@ -9,9 +9,13 @@ public class main {
             System.out.println("Hello world!");
             Scanner scan = new Scanner(System.in);
             switch (scan.nextLine()){
+                case"hello": System.out.println("Hello world!"); break;
                 case "info":
                     System.out.println("Szia"); break;
                 case"exit":System.exit(1); break;
+                case"help": System.out.println("exit:help:info:hello");
+
+
             }
         }
 
